1b15834a1ca6e3ea844ecfa12da19dd4e0ca6e2a
Tuba Tosunoglu <58993751+TUBATOSUNOGLU@users.noreply.github.com>
2022-12-08 19:40:47 +0000

    Lemmatization, removing function for punctuation

--- chatbot/smart-chatbot.py
+++ chatbot/smart-chatbot.py
@@ -15,3 +15,15 @@ word_tokens= nltk.word_tokenize(raw_document) #for converting document to list o
 #examples
 print(sent_tokens[:2])  #first 2 sentences should be printed in whatismentalhealth.txt file
 print(word_tokens[:2])  #first 2 words should be printed in whatismentalhealth.txt file
+
+
+#nltk.org/Text proccessing
+#Lemmatization
+#We have to have regular corpus data! Vectors should be organized
+#Concider when sentences are broke down to he words, what is happening to the punctuation marks?
+lemmer=nltk.stem.WordNetLemmatizer() #Wordnet is a semantically-oriented dictionary of English included nltk
+def LemmaTokens(tokens):
+    return[lemmer.lemmatize(token) for token in tokens]
+remove_punctuation_dict=dict((ord(punct), None) for punct in string.punctuation)
+def LemmaNormalize(text):
+    return LemmaTokens(nltk.word_tokenize(text.lower().translate(remove_punctuation_dict))) 
