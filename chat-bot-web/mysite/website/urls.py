ad8fe2f8ba0a4a54484a2fecd13e21cc44c7abe3
Tuba Tosunoglu <58993751+TUBATOSUNOGLU@users.noreply.github.com>
2022-12-14 16:20:10 +0000

    path is added in urls.py for smartchatbot and sendmessage request

--- chat-bot-web/mysite/website/urls.py
+++ chat-bot-web/mysite/website/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index,name='index'),
-    path("about", views.about,name='about'),
-
+   path("", views.index,name='index'),
+   path("about", views.about,name='about'),
+   path("smartchatbot", views.smartchatbot,name='smartchatbot'),
+   path("sendmessage",views.sendmessage, name="sendmessage"),
+ 
 ]
