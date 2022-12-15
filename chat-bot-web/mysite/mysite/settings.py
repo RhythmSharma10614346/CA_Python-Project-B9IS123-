29c4f6008811a223b416761ae6d260810508a0ae
RhythmSharma10614346 <rhysha.19@gmail.com>
2022-12-15 15:11:44 +0000

    chatbot is visible via navbar

--- chat-bot-web/mysite/mysite/settings.py
+++ chat-bot-web/mysite/mysite/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
